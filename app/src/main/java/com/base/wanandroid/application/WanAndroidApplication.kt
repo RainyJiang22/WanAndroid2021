f5de1719ab11e211511a06b62a721f62526b628e
jiangshiyu <3434481891@qq.com>
2022-05-30 16:05:29 +0800

    首页数据加载更多和刷新

--- app/src/main/java/com/base/wanandroid/application/WanAndroidApplication.kt
+++ app/src/main/java/com/base/wanandroid/application/WanAndroidApplication.kt
@@ -2,11 +2,18 @@ package com.base.wanandroid.application
 
 import android.app.Application
 import android.content.Context
+import android.graphics.pdf.PdfDocument
+import com.base.wanandroid.R
 import com.base.wanandroid.application.initializer.CommonProcessInitializer
 import com.base.wanandroid.application.initializer.DelegateInitializer
 import com.base.wanandroid.application.initializer.MainProcessInitializer
 import com.base.wanandroid.application.initializer.StubProcessInitializer
 import com.base.wanandroid.utils.AppUtils
+import com.drake.brv.PageRefreshLayout
+import com.drake.statelayout.StateConfig
+import com.scwang.smart.refresh.footer.ClassicsFooter
+import com.scwang.smart.refresh.header.ClassicsHeader
+import com.scwang.smart.refresh.layout.SmartRefreshLayout
 
 /**
  * @author jiangshiyu
@@ -23,12 +30,35 @@ class WanAndroidApplication : Application() {
         super.onCreate()
         sContext = this
 
-        mInitializer = if(AppUtils.inMainProcess(this)) {
+        mInitializer = if (AppUtils.inMainProcess(this)) {
             CommonProcessInitializer(newMainProcessInitializer())
         } else {
             CommonProcessInitializer(newStubProcessInitializer())
         }
         mInitializer?.appCreate(this)
+
+
+        //全局缺省页配置
+        StateConfig.apply {
+            emptyLayout = R.layout.layout_empty
+            loadingLayout = R.layout.layout_loading
+            errorLayout = R.layout.layout_error
+        }
+
+        //全局分页索引
+        PageRefreshLayout.startIndex = 0
+        //全局预加载索引
+        PageRefreshLayout.preloadIndex = 3
+
+        //初始化SmartRefreshLayout构建器
+        SmartRefreshLayout.setDefaultRefreshHeaderCreator { context, layout ->
+            layout.setPrimaryColorsId(R.color.color_primary_dark, R.color.white)
+            ClassicsHeader(context)
+        }
+        SmartRefreshLayout.setDefaultRefreshFooterCreator { context, layout ->
+            ClassicsFooter(context).setDrawableSize(20f)
+        }
+
 
     }
 
--- app/src/main/java/com/base/wanandroid/ui/home/HomeFragment.kt
+++ app/src/main/java/com/base/wanandroid/ui/home/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import android.widget.LinearLayout
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import cn.nekocode.rxlifecycle.LifecycleEvent
@@ -16,13 +17,16 @@ import com.base.wanandroid.ui.adapter.ImageTitleAdapter
 import com.base.wanandroid.utils.RxTransformer
 import com.base.wanandroid.utils.lifecycleOwner
 import com.bumptech.glide.Glide
+import com.drake.brv.PageRefreshLayout
 import com.youth.banner.indicator.CircleIndicator
 import com.youth.banner.transformer.ZoomOutPageTransformer
+import kotlinx.coroutines.launch
 import java.util.concurrent.TimeUnit
 
 /**
  * @author jiangshiyu
  * @date 2022/3/7
+ * 主页
  */
 class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>() {
 
@@ -38,42 +42,83 @@ class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>() {
 
     override fun init(savedInstanceState: Bundle?) {
         initView()
-        getArticleList()
         getBannerData()
+        //刷新
+        onRefresh()
+        //加载
+        onLoadMore()
     }
 
 
     private fun initView() {
-
         binding?.rvHomeList?.also {
             it.layoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
             it.adapter = articleAdapter
         }
     }
 
-    /**
-     * 主页数据
-     */
-    private fun getArticleList() {
-        viewModel.getArticleList(0)
-            .timeout(30, TimeUnit.SECONDS)
-            .compose(
-                RxLifecycleCompact.bind(this)
-                    .disposeObservableWhen(LifecycleEvent.DESTROY_VIEW)
-            )
-            .compose(RxTransformer.async())
-            .subscribe({
-                if (BuildConfig.LOG_ENABLE) {
-                    Log.d(TAG, "getHomeData: $it")
-                }
-                //设置文章数据
-                articleAdapter.setDiffNewData(it.data.datas.toMutableList())
 
-            }, {
-                if (BuildConfig.LOG_ENABLE) {
-                    Log.d(TAG, "getHomeDataError: $it")
-                }
-            }).lifecycleOwner(this)
+    /**
+     * 页面刷新操作，刷新回调,下拉刷新
+     */
+    private fun onRefresh() {
+        binding?.pageHome?.onRefresh {
+            lifecycleScope.launch {
+
+                //加载主页数据
+                viewModel.getArticleList(index)
+                    .timeout(30, TimeUnit.SECONDS)
+                    .compose(
+                        RxLifecycleCompact.bind(this@HomeFragment)
+                            .disposeObservableWhen(LifecycleEvent.DESTROY_VIEW)
+                    )
+                    .compose(RxTransformer.async())
+                    .subscribe({
+                        //设置文章数据
+                        articleAdapter.setList(it.data.datas.toMutableList())
+                    }, {
+                        showEmpty()
+                    }).lifecycleOwner(this@HomeFragment)
+                index += 1
+                showContent(true)
+            }
+            //自动刷新
+        }?.refreshing()
+    }
+
+
+    /**
+     * 加载更多
+     */
+    private fun onLoadMore() {
+        binding?.pageHome?.onLoadMore {
+            lifecycleScope.launch {
+                viewModel.getArticleList(index)
+                    .timeout(30, TimeUnit.SECONDS)
+                    .compose(
+                        RxLifecycleCompact.bind(this@HomeFragment)
+                            .disposeObservableWhen(LifecycleEvent.DESTROY_VIEW)
+                    )
+                    .compose(RxTransformer.async())
+                    .subscribe({
+                        if (it.data.datas.isNullOrEmpty()) {
+                            //没有更多内容
+                            showContent(false)
+                            return@subscribe
+                        }
+                        articleAdapter.addData(it.data.datas)
+                        //翻页
+                        index += 1
+                        showContent(true)
+                    }, {
+                        showEmpty()
+                    }).lifecycleOwner(this@HomeFragment)
+
+            }
+
+        }
+
+
     }
 
     /**
@@ -108,6 +153,12 @@ class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>() {
                 }
 
             }).lifecycleOwner(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        //设置页面分页初始索引
+        PageRefreshLayout.startIndex = 0
     }
 
 
